0a53047f69c4648e3cd10d8cb873786a92f393d7
Hachel <944080287@qq.com>
2018-07-27 17:21:17 +0800

    初始化

--- chapter9/eureka-monitor-client/src/main/java/com/forezp/EurekaMonitorClientApplication.java
+++ chapter9/eureka-monitor-client/src/main/java/com/forezp/EurekaMonitorClientApplication.java
@@ -2,10 +2,12 @@ package com.forezp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 @SpringBootApplication
 @EnableTurbine
+@EnableHystrixDashboard
 public class EurekaMonitorClientApplication {
 
 	public static void main(String[] args) {
