36e63162d94d5cabe4e567c4f53a8650f20151d9
Hachel <944080287@qq.com>
2018-07-28 17:57:46 +0800

    初始化

--- chapter13/src/main/java/com/forezp/service/impl/UserService.java
+++ chapter13/src/main/java/com/forezp/service/impl/UserService.java
@@ -17,6 +17,13 @@ public class UserService implements UserDetailsService {
     private UserDao userRepository;
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+       //1.jpa
         return userRepository.findByUsername(username);
+        /*2.mybatis
+            1.自定义CustomUserService实现UserDetailsService接口，重写loadUserByUsername方法
+            2.生成用户权限集合List<SimpleGrantedAuthority> authorities
+            3.生成spring自带的UserDetails
+            new org.springframework.security.core.userdetails.User(user.getUsername(),user.getPassword(), authorities);
+         */
     }
 }
